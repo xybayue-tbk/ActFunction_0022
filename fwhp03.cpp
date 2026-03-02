527e892c730436122447c515a00648e99ca390c6
xybayue-tbk <xybayue@gmail.com>
2026-03-02 12:27:20 +0700

    menghitung + output

---
+++ fwhp03.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int panjang, lebar;
+
+void input()
+{
+    cout << "Masukkan Panjang: ";
+    cin >> panjang;
+    cout << "Masukkan Lebar: ";
+    cin >> lebar;
+}
+
+int luasPersegi()
+{
+    return panjang * lebar;
+}
+
+void output()
+{
+    cout << "Hasilnya: " << luasPersegi();
+}
