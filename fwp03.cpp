f85d148f179b474b9f903902f55addd6f1e11782
xybayue-tbk <xybayue@gmail.com>
2026-03-02 12:14:41 +0700

    perhitungan

---
+++ fwp03.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int panjang, lebar;
+
+void input()
+{
+    cout << "Masukkan panjang: ";
+    cin >> panjang;
+    cout << "Masukkan lebar: ";
+    cin >> lebar;  
+}
+
+int luasPersegi(int a, int b)
+{
+    return a * b;
+}
+
+int kelilingPersegi(int a, int b)
+{
+    return 2 * (a + b);
+}
