892c16cd321906ccc06fe42d00d07acb3a73b31c
xybayue-tbk <xybayue@gmail.com>
2026-03-02 12:13:18 +0700

    input

---
+++ fwp.02.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int panjang, lebar;
+
+void input()
+{
+    cout << "Masukkan panjang: ";
+    cin >> panjang;
+    cout << "Masukkan lebar: ";
+    cin >> lebar;  
+}
