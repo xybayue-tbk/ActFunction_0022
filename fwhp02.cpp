037343a61a20dc069e3ac789ce686a38a21f0c9c
xybayue-tbk <xybayue@gmail.com>
2026-03-02 12:26:04 +0700

    menambahkan imput

---
+++ fwhp02.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int panjang, lebar;
+
+void input()
+{
+    cout << "Masukkan Panjang: ";
+    cin >> panjang;
+    cout << "Masukkan Lebar: ";
+    cin >> lebar;
+}
